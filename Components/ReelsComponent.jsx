93a02dc38e25b7e8442feb205c07837fdde65547
Cristian Facundo Varela <cvarelagarcia@gmail.com>
2022-04-05 19:17:06 -0300

    Modificacion de colores de botones

--- Components/ReelsComponent.jsx
+++ Components/ReelsComponent.jsx
@@ -17,10 +17,10 @@ export default function ReelsComponent(props) {
 
                 <View style={{flexDirection: "column", justifyContent: "space-between", marginVertical: 370, marginLeft: 270, position:"absolute"}}>
                     <View style={{ flexDirection: "column", alignItems: "center"}}>                        
-                        <AntDesign name="hearto" size={24} color="white" style={{ marginTop: 20}} />
-                        <Feather name="message-circle" size={24} color="white" style={{ marginTop: 20 }} />
-                        <Feather name="send" size={24} color="white" style={{ marginTop: 20 }} />
-                        <Text style={{fontWeight: "bold", marginTop: 20, color:"white"}}>{}...</Text>
+                        <AntDesign name="hearto" size={24} color="black" style={{ marginTop: 20}} />
+                        <Feather name="message-circle" size={24} color="black" style={{ marginTop: 20 }} />
+                        <Feather name="send" size={24} color="black" style={{ marginTop: 20 }} />
+                        <Text style={{fontWeight: "bold", marginTop: 20, color:"black"}}>{}...</Text>
                     </View>
                 </View>
                 <Text style={{fontWeight: "bold", fontSize:22, marginLeft: 10}}>{props.likes} Likes</Text>
